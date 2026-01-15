ce4980efdfe06718b1c1060e62a1c6d5c1cc6bc8
Willia135 <gustavowillianoliveira@gmail.com>
2026-01-15 16:19:51 -0300

    Refactor video transcription script for clarity

--- Projeto transcrever videos com IA/Projeto_videosIA.py
+++ Projeto transcrever videos com IA/Projeto_videosIA.py
@@ -10,20 +10,21 @@ video_url: str = input("Coloque o link do vídeo do YouTube: ")
 print("Você digitou:", video_url)
 
 # Verifica se o link é válido
+
 if ("youtube.com" in video_url) or ("youtu.be" in video_url):
-    print("✅ Link válido!")
+    print("Link válido!")
 else:
-    print("❌ Link inválido!")
+    print("Link inválido!")
     exit(1)
 
 
 # Download do áudio
 
-print("⬇️ Baixando o vídeo do YouTube...")
+print("Baixando o vídeo do YouTube...")
 yt = YouTube(video_url)
 stream = yt.streams.filter(only_audio=True).first()  # Baixa apenas o áudio
 stream.download(filename="video.mp4") #Salva o arquivo do audio como video.mp4
-print("✅ Download concluído: video.mp4")
+print("Download concluído: video.mp4")
 
 
 #Converter para MP3 (com FFmpeg)
@@ -32,22 +33,24 @@ print("🎧 Convertendo para MP3...")
 comando_ffmpeg: str = f'ffmpeg -i "video.mp4" -q:a 0 -map a "audio.mp3" -y'
 ret = os.system(comando_ffmpeg)
 if ret != 0:
-    print("❌ Erro ao converter com FFmpeg.")
+    print("Erro ao converter com FFmpeg.")
     exit(1)
-print("✅ Conversão concluída: audio.mp3")
+print("Conversão concluída: audio.mp3")
 
 
 #Transcrever o áudio com Whisper local
 
-print("🧠 Transcrevendo o áudio com Whisper local...")
+print("Transcrevendo o áudio com Whisper")
 modelo = whisper.load_model("base")  # Opções: tiny, base, small, medium, large
 resultado = modelo.transcribe("audio.mp3")
 
 texto_transcrito: str = resultado["text"]
-print("✅ Transcrição concluída!")
+print("Transcrição concluída")
 
 #Salva a transcrição do video em um arquivo
+
 with open("transcricao.txt", "w", encoding="utf-8") as f:
     f.write(texto_transcrito)
-print("📝 Transcrição salva em 'transcricao.txt'.")
+print("Transcrição salva em 'transcricao.txt'.")
+
 
